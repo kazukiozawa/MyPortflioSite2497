534c50ef60b1bbf8f15483187bafbbab8718a23a
kazuki_ozawa <78969977+kazukiozawa@users.noreply.github.com>
2024-09-07 14:31:20 +0900

    Add files via upload

---
+++ scripts/libs/gsap-test.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.getElementById('menu-btn');
+    const spans = menuBtn.querySelectorAll('span');
+    let isOpen = false;
+  
+    menuBtn.addEventListener('click', () => {
+      if (!isOpen) {
+        gsap.to(spans[0], { y: 3, rotation: 45, duration: 0.4 });
+        gsap.to(spans[1], { opacity: 0, duration: 0.4 });
+        gsap.to(spans[2], { y: -3, rotation: -45, duration: 0.4 });
+        gsap.to(menuBtn, { backgroundColor: '#1565e', duration: 0.4 });
+    } else {
+        gsap.to(spans[0], { y: 0, rotation: 0, duration: 0.6 });
+        gsap.to(spans[1], { opacity: 1, duration: 0.6 });
+        gsap.to(spans[2], { y: 0, rotation: 0, duration: 0.6 });
+        gsap.to(menuBtn, { backgroundColor: '#ffffff', duration: 0.4 });
+    }
+      isOpen = !isOpen;
+    });
+  });
+  
